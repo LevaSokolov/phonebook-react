55d6fe3a65d4b578fdce702ff34ee7f6f3b38dce
Michael Mironychev <michael.mironychev@gmail.com>
2022-09-03 11:41:52 +0700

    some learning

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import ClassCounter from "./components/ClassCounter";
+import Counter from "./components/Counter";
+
+function App() {
+  const [value, setValue] = useState('input text')
+
+  return (
+    <div className="App">
+     <Counter/>
+     <ClassCounter />
+    </div>
+  );
+}
+
+export default App;

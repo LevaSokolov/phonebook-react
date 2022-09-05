de7d8a366714f6d6a3556f21e094f39c942b9ed7
Michael Mironychev <michael.mironychev@gmail.com>
2022-09-05 19:46:57 +0700

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import ClassCounter from "./components/ClassCounter";
 import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.css'
 
 function App() {
-  const [value, setValue] = useState('input text')
+  // const [value, setValue] = useState('input text')
 
   return (
     <div className="App">
-     <Counter/>
-     <ClassCounter />
+      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}} />
     </div>
   );
 }

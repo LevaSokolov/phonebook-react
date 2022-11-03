ac22127c35a2bf2552e6753350f4f3e0350aea79
lionsokolov-admin <leva1sokolov@gmail.com>
2022-11-04 00:00:46 +0700

    notfoundpage style fix

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,15 @@
+import '../styles/NotFoundPage.scss';
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Button from '../components/UI/Button';
 
 const NotFoundPage = () => (
-    <div style={{ margin: '30px' }}>
-      <div style={{ fontSize: '32px' }}>PAGE NOT FOUND...</div>
+    <div className='main'>
+      <div>PAGE NOT FOUND...</div>
       <Link to="/login">
-        <Button type="button" style={{ fontSize: '32px', margin: '10px', width: '260px' }}>GO BACK</Button>
+        <Button className='button'>GO BACK</Button>
       </Link>
     </div>
 );
